87dd6edd6cf2e55a94ff4b81cc792f5dc02fcd93
BrianDyl <brianadyl@aol.com>
2021-12-23 10:37:22 -0500

    Added Leaflet map library

--- script.js
+++ script.js
@@ -11,4 +11,43 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-alert('new feature');
+if(navigator.geolocation)
+navigator.geolocation.getCurrentPosition(function(position){
+
+    const latitude = position.coords.latitude;
+
+    //using object destructuring 
+    const {longitude} = position.coords;
+    
+    const coords = [latitude, longitude];
+   
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    
+
+    map.on('click', function(mapEvent){
+
+        const { lat, lng} = mapEvent.latlng;
+
+        L.marker([lat, lng]).addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+        }))
+        .setPopupContent('Workout')
+        .openPopup();
+    });
+    }, 
+    function(){
+        alert('Could not get your position');  
+    }
+);
+
+// pk.eyJ1IjoiYnJpYW5keWwiLCJhIjoiY2t4ajFkMmFyMjl6NzJvb2tnbmt4YjB2NiJ9.AqtwS0RJHdwbIkgG3PLw5w
